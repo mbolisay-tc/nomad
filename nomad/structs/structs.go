0a05eda1d4d1e445275f79db1f5a91d9b9bce52b
Drew Bailey <2614075+drewbailey@users.noreply.github.com>
2020-10-05 16:24:09 -0400

    durable events, temp go-memdb dep

--- nomad/structs/structs.go
+++ nomad/structs/structs.go
@@ -10682,3 +10682,18 @@ func NewRpcError(err error, code *int64) *RpcError {
 func (r *RpcError) Error() string {
 	return r.Message
 }
+
+type Topic string
+type Event struct {
+	Topic      Topic
+	Type       string
+	Key        string
+	FilterKeys []string
+	Index      uint64
+	Payload    interface{}
+}
+
+type Events struct {
+	Index  uint64
+	Events []Event
+}
